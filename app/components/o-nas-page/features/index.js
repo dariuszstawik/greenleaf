032aee3a0f799c7fc7402a4183387eb9de809e4b
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-10-13 19:41:33 +0200

    Fix rwd in about page

--- app/components/o-nas-page/features/index.js
+++ app/components/o-nas-page/features/index.js
@@ -6,7 +6,7 @@ export default function Features() {
   return (
     // <section className="bg-primaryGray lg:h-screen">
     <section
-      className="mt-28 relative w-screen px-32 py-20 pb-28 bg-primary after:content-[''] after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-secondaryGreen after:opacity-90"
+      className="mt-28 relative w-screen px-10 lg:px-32 py-20 pb-28 bg-primary after:content-[''] after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-secondaryGreen after:opacity-90"
       style={{
         backgroundImage: 'url("/glp_slider20.jpg")',
         backgroundPosition: "center",
@@ -19,7 +19,7 @@ export default function Features() {
         <Fade direction="bottom" delay={600} triggerOnce>
           <div className="flex flex-wrap -mx-3 -mb-10 lg:-mb-16  z-20">
             <div className="w-full md:w-1/2 lg:w-1/3 px-3 mb-10 lg:mb-16">
-              <div className="relative pt-16 pb-12 px-8 rounded-lg text-center">
+              <div className="relative pt-16 pb-12 px-2 lg:px-8 rounded-lg text-center">
                 <span className="absolute top-0 inset-x-0 -mt-6 flex justify-center items-center w-16 h-16 mx-auto rounded-full bg-primaryGreen text-white">
                   1
                 </span>
