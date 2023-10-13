699a850c9dbd7b1e83d7e92e4329b2b9e90b2c85
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-10-13 19:46:26 +0200

    Fix rwc

--- app/components/o-nas-page/features/index.js
+++ app/components/o-nas-page/features/index.js
@@ -33,7 +33,7 @@ export default function Features() {
             </div>
 
             <div className="w-full md:w-1/2 lg:w-1/3 px-3 mb-10 lg:mb-16">
-              <div className="relative pt-16 pb-12 px-8 rounded-lg text-center">
+              <div className="relative pt-16 pb-12 px-2 lg:px-8 rounded-lg text-center">
                 <span className="absolute top-0 inset-x-0 -mt-6 flex justify-center items-center w-16 h-16 mx-auto rounded-full bg-primaryGreen text-white">
                   2
                 </span>
@@ -45,7 +45,7 @@ export default function Features() {
               </div>
             </div>
             <div className="w-full md:w-1/2 lg:w-1/3 px-3 mb-10 lg:mb-16">
-              <div className="relative pt-16 pb-12 px-8 rounded-lg text-center">
+              <div className="relative pt-16 pb-12 px-2 lg:px-8 rounded-lg text-center">
                 <span className="absolute top-0 inset-x-0 -mt-6 flex justify-center items-center w-16 h-16 mx-auto rounded-full bg-primaryGreen text-white">
                   3
                 </span>
