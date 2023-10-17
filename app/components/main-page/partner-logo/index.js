72e9b5a7cd5567e297f0c6261767e2a1c578d113
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-10-17 11:48:55 +0200

    Fix error with images

--- app/components/main-page/partner-logo/index.js
+++ app/components/main-page/partner-logo/index.js
@@ -7,7 +7,7 @@ export default function PartnerLogo({ logo }) {
   return (
     <div className="w-1/2 md:w-1/3 lg:w-1/5 px-4 mb-8 lg:mb-0">
       <div className="flex items-center h-32 md:h-36 px-4 md:px-8 rounded-md border">
-        <Image
+        <img
           className="mx-auto"
           src={`https://${logo.fields.file.url}`}
           alt={logo.fields.description}
