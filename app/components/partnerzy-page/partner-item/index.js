b504c5a86a22ff54d2e86ce2d85cb964e90abe72
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-10-17 12:18:05 +0200

    Configure Contentful on pages

---
+++ app/components/partnerzy-page/partner-item/index.js
@@ -0,0 +1,56 @@
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import ListItem from "../../global-components/list-item";
+
+export default function Partneritem({
+  title,
+  content,
+  list1,
+  list2,
+  list3,
+  img,
+  imgLink,
+}) {
+  return (
+    <>
+      <div className="max-w-6xl mx-auto flex flex-col justify-center items-center lg:flex-row gap-6 mt-20 mb-10">
+        <div className="w-1/2">
+          <h4>{title}</h4>
+          <div>{documentToReactComponents(content)}</div>
+          <ul>
+            {list1 && (
+              <ListItem className="border-b-0 pb-1 px-0">{list1}</ListItem>
+            )}
+            {list2 && (
+              <ListItem className="border-b-0 pb-1 px-0">{list2}</ListItem>
+            )}
+            {list3 && (
+              <ListItem className="border-b-0 pb-1 px-0">{list3}</ListItem>
+            )}
+
+            {/* <ListItem className="border-b-0 pb-1 px-0">
+            Growatt: inwertery, magazyny energii
+          </ListItem>
+          <ListItem className="border-b-0 pb-1 px-0">
+            Atess: inwertery, ładowarki do samochodów
+          </ListItem>
+          <ListItem className="border-b-0 pb-1 px-0">
+            Hopergy: konstrukcje
+          </ListItem> */}
+          </ul>
+        </div>
+        <div className="w-1/2 flex justify-center">
+          <a href={imgLink} target="_blank">
+            <img
+              src={img.fields.file.url ? img.fields.file.url : ""}
+              alt={
+                img.fields.file.description ? img.fields.file.description : ""
+              }
+              className="w-[360px]"
+            />
+          </a>
+        </div>
+      </div>
+      <hr className="w-full mt-10" />
+    </>
+  );
+}
--- app/partnerzy/page.js
+++ app/partnerzy/page.js
@@ -1,47 +1,42 @@
+import { client } from "@/lib/contentful/client";
 import ListItem from "../components/global-components/list-item";
 import PageHeader from "../components/global-components/page-header";
 import SectionTitle from "../components/global-components/section-title";
+import Partneritem from "../components/partnerzy-page/partner-item";
 
-export default function Partnerzy() {
+async function getContentfulPartners() {
+  const resPartners = await client.getEntries({
+    content_type: "patner",
+  });
+
+  const partners = resPartners.items;
+
+  return partners;
+}
+
+export default async function Partnerzy() {
+  const partners = await getContentfulPartners();
+
   return (
     <>
       <PageHeader>Partnerzy</PageHeader>
       <section className=" mx-auto px-8 mb-16 pt-10">
         <SectionTitle>Współpraca</SectionTitle>
-        <div className="max-w-6xl mx-auto flex flex-col justify-center items-center lg:flex-row gap-6 mt-20 mb-10">
-          <div className="w-1/2">
-            <h4>MP Solar Group</h4>
-            <ul>
-              wyłączny przedstawiciel na Polskę produktów:
-              <ListItem className="border-b-0 pb-1 px-0">
-                Growatt: inwertery, magazyny energii
-              </ListItem>
-              <ListItem className="border-b-0 pb-1 px-0">
-                Atess: inwertery, ładowarki do samochodów
-              </ListItem>
-              <ListItem className="border-b-0 pb-1 px-0">
-                Hopergy: konstrukcje
-              </ListItem>
-            </ul>
-          </div>
-          <div className="w-1/2 flex justify-center">
-            <a href="https://www.mpsolar.pl/" target="_blank">
-              <img src="MP-Solar_logo.png" alt="" className="w-[360px]" />
-            </a>
-          </div>
-        </div>
-        <hr className="w-screen" />
-        <div className="max-w-6xl mx-auto flex flex-col justify-center items-center lg:flex-row gap-6 mt-20 mb-10">
-          <div className="w-1/2">
-            <h4>RNG Project</h4>
-            <p>partner w zakresie rozwiązań przemysłowych magazynów energii</p>
-          </div>
-          <div className="w-1/2 flex justify-center">
-            <a href="https://nrgproject.pl/" target="_blank">
-              <img src="NRG_logo.png" alt="" className="w-[360px]" />
-            </a>
-          </div>
-        </div>
+
+        {partners.map((item, i) => {
+          return (
+            <Partneritem
+              key={i}
+              title={item.fields.title}
+              content={item.fields.content}
+              list1={item.fields.list1}
+              list2={item.fields.list2}
+              list3={item.fields.list3}
+              img={item.fields.logo ? item.fields.logo : ""}
+              imgLink={item.fields.logoLink ? item.fields.logoLink : ""}
+            />
+          );
+        })}
       </section>
     </>
   );
